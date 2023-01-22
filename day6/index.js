066a51133540e03cf6cf41a96b114f34a989be6f
zhiming <zhiming.hao@qq.com>
2023-01-22 12:37:25 +0800

    第6天  删除有序数组中的重复项-使用step

--- day6/index.js
+++ day6/index.js
@@ -2,6 +2,9 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// 比较当前元素和下一位元素是否重复，重复则删除下一位，循环位置不变，继续与下一个元素比较
+// 当前元素和下一个原属不重复，循环位置递增1，移动下一位
 let removeDuplicates = function(nums) {
 
     let step = 0
