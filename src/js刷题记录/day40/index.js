6188f6fbc75de4573e1ed01337154083b483a051
之铭 <zhiming.hao@qq.com>
2023-03-24 23:06:43 +0800

    up

--- src/js刷题记录/day40/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-let search = function (nums, target) {
-    return nums.indexOf(target)
-};
-console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
